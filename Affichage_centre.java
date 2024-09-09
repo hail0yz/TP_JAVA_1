0a85c00a3a927d75fab73f34bb822b13b9643c7b
Vito ☻ <117570651+hail0yz@users.noreply.github.com>
2024-09-09 14:10:40 +0200

    Add files via upload

---
+++ Affichage_centre.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class TexteCentre{
+	private String texte;
+	private int largeur;
+	TexteCentre(String texte){
+		this.texte= texte;
+		this.largeur= ;
+	}
+	private int calculLargeur(){
+		
+	}
+	void fixeLargeur(int l){
+        this.largeur = l;
+	}
+	String texte(){
+		return this.texte;
+	}
+	@Override
+	public String toString(){
+
+	}
+}
